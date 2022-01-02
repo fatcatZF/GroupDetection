745775edb869519a05d779d788ca0eb0d2183d5a
ZFang <z.fang0512@gmail.com>
2022-01-02 23:16:57 +0100

    typed GCN autoencoder

---
+++ models_gcn.py
@@ -0,0 +1,95 @@
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from utils import *
+
+
+
+class GCNLayer(nn.Module):
+    def __init__(self, n_in, n_out):
+        super(GCNLayer, self).__init__()
+        self.fc = nn.Linear(n_in, n_out)        
+        self.init_weights()
+                
+    def init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.xavier_normal_(m.weight.data)
+                m.bias.data.fill_(0.1)
+        
+    def forward(self, A_norm, inputs=None):
+        """
+        A_norm: normalized adjacency matrix
+           shape: [batch_size, num_atoms, num_atoms]
+        inputs: attributes matrix of nodes
+           shape: [batch_size, num_atoms, num_atoms]
+           if inputs==None:
+               only consider structure information
+        """
+        if inputs is None:
+            I = torch.eye(A_norm.size(-1))
+            inputs = I.unsqueeze(0).expand(A_norm.size(0),I.size(0),I.size(1))
+        
+        return torch.matmul(A_norm, self.fc(inputs))
+    
+
+
+
+
+
+class TypedGCNAutoEncoder(nn.Module):
+    """2 layer Typed GCN Encoder"""
+    def __init__(self, n_in, n_hid, n_out, edge_types, skip_first=True,
+                 mode="encoder"):
+        super(TypedGCNEncoder, self).__init__()
+        #first layer of GCNs
+        self.gcns_1 = nn.ModuleList(
+            [GCNLayer(n_in, n_hid) for _ in range(edge_types)])
+        self.gcns_2 = nn.ModuleList(
+            [GCNLayer(n_hid, n_out)] for _ in range(edge_types))
+        
+        self.mode = mode
+        self.skip_first = skip_first
+        self.n_in = n_in
+        self.n_hid = n_hid
+        self.n_out = n_out
+        
+    def forward(self, A_norm, inputs=None):
+        """
+        A_norm: normalized adjacency matrix
+            shape: [batch_size, num_edgetypes, num_atoms, num_atoms]
+        inputs: node attributes
+             shape: [batch_size, num_atoms, n_dimension]
+             if None, Identity matrix will be used:
+                 [batch_size, num_atoms, num_atoms]
+        """
+        if inputs is None:
+            I = torch.eye(A_norm.size(-1))
+            inputs = I.unsqueeze(0).expand(A_norm.size(0),I.size(0),I.size(1))
+            
+        if self.skip_first:
+            start_idx = 1
+        else:
+            start_idx = 0
+        
+            
+        all_outputs = torch.zeros(inputs.size(0), inputs.size(1), self.n_out)
+        #shape: [batch_size, num_atoms, n_out]
+        
+        for i in range(start_idx, len(self.gcns_1)):
+            hidden = F.relu(self.gcns_1[i](inputs))
+            output = self.gcns_2[i](hidden)
+            all_outputs += output
+            
+        return all_outputs
+            
+        
+        
+
+
+            
+        
+        
+        
+
